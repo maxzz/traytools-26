18b937b1ff250e6ed948f953e167e1e7cdbad9da
Max Zakharzhevskiy <maxzz@msn.com>
2026-07-03 15:08:53 -0700

    WIP. code review. files layout

--- frontend/src/components/0-all/8-pages-array.tsx
+++ frontend/src/components/0-all/8-pages-array.tsx
@@ -1,17 +1,17 @@
 import { type ComponentType } from "react";
 import { PageDemos } from "../2-main/xyz-demos";
-import { PageTestTabA } from "../2-main/tab-tests/01-page-a/0-page-a-all";
-import { PageTestTabB } from "../2-main/tab-tests/02-page-b/0-page-b-all";
-import { PageWelcome } from "../2-main/tab-welcome/0-welcome-all";
-import { PageTraceManager } from "../2-main/tab-tracemanager/0-trace-manager-all";
-import { PageWindowsTree } from "../2-main/tab-windowstree/0-windows-tree-all";
-import { PageToolsEditor } from "../2-main/tab-tools-editor/0-tools-editor-all";
+import { PageTestTabA } from "../2-main/xyz-demos/01-page-a/0-page-a-all";
+import { PageTestTabB } from "../2-main/xyz-demos/02-page-b/0-page-b-all";
+import { PageWelcome } from "../2-main/8-tab-welcome/0-welcome-all";
+import { PageTraceManager } from "../2-main/2-tab-trace-bits/0-trace-manager-all";
+import { PageWindowsTree } from "../2-main/1-tab-windows-tree/0-windows-tree-all";
+import { PageToolsEditor } from "../2-main/3-tab-tools-editor/0-tools-editor-all";
 
 export const MAIN_PAGES = [
     { id: "welcome", label: "Welcome Screen", Page: PageWelcome },
-    { id: "trace", label: "Trace Manager", Page: PageTraceManager },
+    { id: "trace-bits", label: "Trace Manager", Page: PageTraceManager },
     { id: "windows-tree", label: "Windows Tree", Page: PageWindowsTree },
-    { id: "tools-editor", label: "Tools Menu Editor", Page: PageToolsEditor },
+    { id: "tools-menu-editor", label: "Tools Menu Editor", Page: PageToolsEditor },
     { id: "demos", label: "Demos", Page: PageDemos },
     // { id: "test-a", label: "Test A", Page: PageTestTabA },
     // { id: "test-b", label: "Test B", Page: PageTestTabB },
