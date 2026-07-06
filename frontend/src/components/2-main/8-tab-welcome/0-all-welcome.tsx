05143f18b6bf8751abe8523f51de528acad36c46
Max Zakharzhevskiy <maxzz@msn.com>
2026-07-05 21:09:09 -0700

    WIP. update the welcome page

--- frontend/src/components/2-main/8-tab-welcome/0-all-welcome.tsx
+++ frontend/src/components/2-main/8-tab-welcome/0-all-welcome.tsx
@@ -4,14 +4,15 @@ import { envBuildVersion, envModifiedDate } from "@/utils";
 export function PageWelcome() {
     return (
         <div className="flex-1 p-6 min-h-0 flex items-center justify-center">
-            <div className="max-w-lg w-full text-center flex flex-col items-center gap-6">
+            <div className="max-w-lg w-full text-center flex flex-col items-center gap-1">
                 <img src={appIcon} alt="" className="size-24 drop-shadow-sm" />
 
-                <div className="flex flex-col gap-2">
-                    <p className="text-[0.7rem] text-muted-foreground/80">
-                        <div>Version {envBuildVersion()}</div>
-                        <div>{envModifiedDate()}</div>
-                    </p>
+                <div className="flex flex-col gap-1">
+                    Welcome to traytools!
+                    <div className="text-[0.6rem] text-muted-foreground/80 flex gap-2">
+                        <div>Version: {envBuildVersion()}</div>
+                        <div>Updated: {envModifiedDate()}</div>
+                    </div>
                 </div>
             </div>
         </div>
