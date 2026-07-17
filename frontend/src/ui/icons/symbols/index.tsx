a5473f0c4d5ec81fd2657812e2ed85367a1f404f
Max Zakharzhevskiy <maxzz@msn.com>
2026-07-17 15:39:17 -0700

    WIP. windows. only add symbols from win-watch-26-go

--- frontend/src/ui/icons/symbols/index.tsx
+++ frontend/src/ui/icons/symbols/index.tsx
@@ -1,15 +1,21 @@
 import { UISymbolDefsInject } from "./symbols-inject";
 import { DefAppTypes } from "./app";
 import { DefAllOtherTypes } from "./all-other";
+import { DefUiAutomationTypes } from "./ui-automation";
+import { DefControlTypes } from "./controls";
 
 export * from "./app";
 export * from "./all-other";
+export * from "./controls";
+export * from "./ui-automation";
 
 export function UISymbolDefs() {
     return (
         <UISymbolDefsInject>
             {DefAppTypes()}
             {DefAllOtherTypes()}
+            {DefUiAutomationTypes()}
+            {DefControlTypes()}
         </UISymbolDefsInject>
     );
 }
